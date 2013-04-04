b8b630cad47d93648ee7619223e139d1fc3b5fb3
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-04-04 17:30:16 -0400

    updated js file

--- CDVOSXBrowser.xctemplate/___FILEBASENAME___.js
+++ CDVOSXBrowser.xctemplate/___FILEBASENAME___.js
@@ -25,85 +25,57 @@
  */
 
 (function() {
-
-var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
-
-function ___FILEBASENAME___() {
-    // Does nothing
-}
-
-// Callback when the location of the page changes
-// called from native
-___FILEBASENAME___._onLocationChange = function(newLoc)
-{
-    window.plugins.___FILEBASENAME___.onLocationChange(newLoc);
-};
-
-// Callback when the user chooses the 'Done' button
-// called from native
-___FILEBASENAME___._onClose = function()
-{
-    window.plugins.___FILEBASENAME___.onClose();
-};
-
-// Callback when the user chooses the 'open in Safari' button
-// called from native
-___FILEBASENAME___._onOpenExternal = function()
-{
-    window.plugins.___FILEBASENAME___.onOpenExternal();
-};
-
-// Pages loaded into the ___FILEBASENAME___ can execute callback scripts, so be careful to
-// check location, and make sure it is a location you trust.
-// Warning ... don't exec arbitrary code, it's risky and could fuck up your app.
-// called from native
-___FILEBASENAME___._onJSCallback = function(js,loc)
-{
-    // Not Implemented
-    //window.plugins.___FILEBASENAME___.onJSCallback(js,loc);
-};
-
-/* The interface that you will use to access functionality */
-
-// Show a webpage, will result in a callback to onLocationChange
-___FILEBASENAME___.prototype.showWebPage = function(loc)
-{
-    cordovaRef.exec("___FILEBASENAME___.showWebPage", loc);
-};
-
-// close the browser, will NOT result in close callback
-___FILEBASENAME___.prototype.close = function()
-{
-    cordovaRef.exec("___FILEBASENAME___.close");
-};
-
-// Not Implemented
-___FILEBASENAME___.prototype.jsExec = function(jsString)
-{
-    // Not Implemented!!
-    //PhoneGap.exec("___FILEBASENAME___.jsExec",jsString);
-};
-
-// Note: this plugin does NOT install itself, call this method some time after deviceready to install it
-// it will be returned, and also available globally from window.plugins.childBrowser
-___FILEBASENAME___.install = function()
-{
-    if(!window.plugins) {
-        window.plugins = {};
-    }
-        if ( ! window.plugins.___FILEBASENAME___ ) {
-        window.plugins.___FILEBASENAME___ = new ___FILEBASENAME___();
-    }
-
-};
-
-
-if (cordovaRef && cordovaRef.addConstructor) {
-    cordovaRef.addConstructor(___FILEBASENAME___.install);
-} else {
-    console.log("___FILEBASENAME___ Cordova Plugin could not be installed.");
-    return null;
-}
-
-
-})();
+ 
+ var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+ 
+ function ___FILEBASENAME___() {}
+ 
+ 
+ ___FILEBASENAME___.prototype.showWebPage = function(success, fail, object) {
+ 
+ cordovaRef.exec(success, fail, "___FILEBASENAME___", "showWebPage", [object]);
+ 
+ }
+ 
+ ___FILEBASENAME___.prototype.init = function(success, fail, object) {
+ 
+ cordovaRef.exec(success, fail, "___FILEBASENAME___", "init", [object]);
+ 
+ }
+ 
+ 
+ ___FILEBASENAME___.prototype.callNativeFunction = function(success, fail, object) {
+ 
+ cordovaRef.exec(success, fail, "___FILEBASENAME___", "nativeFunction", [object]);
+ 
+ }
+ 
+ ___FILEBASENAME___.prototype.nativePluginResultHandler = function(object) {
+ console.log(object + ' prototyped callback');
+ }
+ 
+ 
+ ___FILEBASENAME___.prototype.nativePluginErrorHandler = function(object) {
+ console.log(object + ' prototyped callback');
+ }
+ 
+ 
+ 
+ ___FILEBASENAME___.install = function() {
+ if (!window.plugins) {
+ window.plugins = {};
+ }
+ if (!window.plugins.___FILEBASENAME___) {
+ window.plugins.___FILEBASENAME___ = new ___FILEBASENAME___();
+ }
+ }
+ 
+ if (cordovaRef && cordovaRef.addConstructor) {
+ cordovaRef.addConstructor(___FILEBASENAME___.install);
+ } else {
+ console.log("___FILEBASENAME___ Cordova Plugin could not be installed.");
+ return null;
+ }
+ 
+ 
+ })();
